026a67bc90c1b0a1ed706408e4262ae76ec726ae
anurag <anurag@pictu.in>
2025-02-04 23:07:52 +0530

    Shippable MVP

--- app/src/main/java/com/esi/mahina/activities/MainActivity.java
+++ app/src/main/java/com/esi/mahina/activities/MainActivity.java
@@ -10,15 +10,10 @@ import androidx.cardview.widget.CardView;
 
 import com.esi.mahina.R;
 import com.esi.mahina.activities.doctorActivities.DoctorOptions;
-import com.esi.mahina.activities.motherActivities.MotherActivity;
-import com.esi.mahina.activities.patientActivities.PatientActivity;
 
 public class MainActivity extends AppCompatActivity {
-//    ActivityMainBinding binding;
 
     private final String DOCTOR = "doctor";
-    private final String PATIENT = "patient";
-    private final String MOTHER = "mother";
     private String userType;
 
     @Override
@@ -27,9 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
 
         CardView cardNavigateDoctorScreen = findViewById(R.id.doctorCard);
         cardNavigateDoctorScreen.setOnClickListener(new View.OnClickListener() {
@@ -40,29 +32,6 @@ public class MainActivity extends AppCompatActivity {
                 userType = DOCTOR;
             }
         });
-
-        CardView cardNavigatePatientScreen = findViewById(R.id.patientCard);
-        cardNavigatePatientScreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, PatientActivity.class);
-                startActivity(intent);
-                userType = PATIENT;
-            }
-        });
-
-
-        CardView cardNavigateMotherScreen = findViewById(R.id.motherCard);
-        cardNavigateMotherScreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MotherActivity.class);
-                startActivity(intent);
-            }
-        });
-
-//        this.userType = String.valueOf(findViewById(R.id.doctorCard));
-
 
     }
 
--- app/src/main/java/com/esi/mahina/activities/motherActivities/MotherActivity.java
+++ app/src/main/java/com/esi/mahina/activities/motherActivities/MotherActivity.java
@@ -1,16 +1,16 @@
-package com.esi.mahina.activities.motherActivities;
-
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.esi.mahina.R;
-
-public class MotherActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mother);
-    }
-}
+//package com.esi.mahina.activities.motherActivities;
+//
+//import android.os.Bundle;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import com.esi.mahina.R;
+//
+//public class MotherActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_mother);
+//    }
+//}
